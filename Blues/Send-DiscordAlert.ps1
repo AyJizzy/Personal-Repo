460d2b474d9348c169b8a12af21f475973568bc9
AyJizzy <ameatte91@gmail.com>
2020-12-30 18:28:25 -0600

    adding option for cardinals tickets

--- Blues/Send-DiscordAlert.ps1
+++ Blues/Send-DiscordAlert.ps1
@@ -1,20 +1,44 @@
-function Send-DiscordAlert ($Uri) 
+function Send-DiscordAlert 
 {
+    Param(
+        [Parameter(Mandatory=$false)]
+        [Switch]$Blues
+        )
     $VerbosePreference = "continue"
-    $Uri = "https://discord.com/api/webhooks/790821392667901992/Xmm7vNm5f4F1voSEvh0dBOxllpHjMdaMJXbxCfYQSSMQoFcHfAOHTAaDiD8VADmZE6WS"
-    Write-Warning -Message "Looking for Discord module"
-    $InstalledModules = Get-InstalledModule -Name PSdiscord
-    
-    If ($InstalledModules)
+    if ($Blues -eq $true)
     {
-        Send-DiscordMessage -uri $uri -message "Blues tickets are available!!!!"
+        $Uri = "https://discord.com/api/webhooks/790821392667901992/Xmm7vNm5f4F1voSEvh0dBOxllpHjMdaMJXbxCfYQSSMQoFcHfAOHTAaDiD8VADmZE6WS"
+        Write-Verbose -Message "Looking for Discord module"
+        $InstalledModules = Get-InstalledModule -Name PSdiscord
+        
+        If ($InstalledModules)
+        {
+            Send-DiscordMessage -uri $uri -message "Blues tickets are available!!!!"
+        }
+        else 
+        {
+            install-module -Name PSDiscord -AcceptLicense -Force
+            Update-Module PSDiscord
+            Import-Module -Name PSDiscord
+            Send-DiscordMessage -uri $uri -message "Blues Tickets are available!!!!"    
+        }
     }
     else 
     {
-        install-module -Name PSDiscord -AcceptLicense -Force
-        Update-Module PSDiscord
-        Import-Module -Name PSDiscord
-        Send-DiscordMessage -uri $uri -message "Blues Tickets are available!!!!"    
+        $Uri = "https://discordapp.com/api/webhooks/793996559658385418/nlJxPsfFVd7QVPXBvbf8CAtm2NsOgTZiZDyIRQSR9CkGAegPcPqmIbJDIOI9frIJHLe_"
+        Write-Verbose -Message "Looking for Discord module"
+        $InstalledModules = Get-InstalledModule -Name PSdiscord
+        If ($InstalledModules)
+        {
+            Send-DiscordMessage -uri $uri -message "Cards tickets are available!!!!"
+        }
+        else 
+        {
+            install-module -Name PSDiscord -AcceptLicense -Force
+            Update-Module PSDiscord
+            Import-Module -Name PSDiscord
+            Send-DiscordMessage -uri $uri -message "Cards Tickets are available!!!!"    
+        }
     }
-}
 
+}
